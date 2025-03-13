034a6ca0a9989946ac7fc44dea8dfb26bd55b50a
zaffinabrianna <zaffinabrianna@yahoo.com>
2025-03-12 19:33:21 -0700

    UUUUUUUUUUUUUUUUUUUUUUUUU

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    
+    deckGroups temp;
+    vector<int> hand = {1, 2, 3, 6, 2, 3, 4, 7, 8};
+    int groupSize = 3;
+
+    temp.validGroup(hand, groupSize);
 
     return 0;
 }
