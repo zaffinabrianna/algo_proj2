d2d51ed21c63e661a76140f0a0bf9f3fe880a23d
Brianna Zaffina <83510513+zaffinabrianna@users.noreply.github.com>
2025-03-20 15:58:33 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     deckGroups temp;
-
+    cout << "Algorithm 2 Test: " << endl;   
     //First Test (should be valid)
     vector<int> hand = { 1, 2, 3, 6, 2, 3, 4, 7, 8 };
     int groupSize = 3;
@@ -11,10 +11,25 @@ int main()
     temp.validGroup(hand, groupSize);
 
     //Second Test (should be invalid)
+    cout << "\n";
     vector<int> hand1 = { 1, 2, 3, 3, 4, 5, 6, 7 };
     int groupSize1 = 4;
     cout << "Second Test [1, 2, 3, 3, 4, 5, 6, 7]:\n";
     temp.validGroup(hand1, groupSize1);
+
+    //Third Test (should be invalid)
+    cout << "\n";
+    vector<int> hand2 = { 1, 2, 3, 3, 4, 5, 6, 7 };
+    int groupSize2 = 5;
+    cout << "Third Test (Group Size 5) [1, 2, 3, 3, 4, 5, 6, 7]:\n";
+    temp.validGroup(hand2, groupSize2);
+
+    //Fourth Test (should be valid)
+    cout << "\n";
+    vector<int> hand3 = {1, 3, 6, 2, 4, 2, 3, 8, 7};
+    int groupSize3 = 3;
+    cout << "Fourth Test [1, 3, 6, 2, 4, 2, 3, 8, 7]:\n";
+    temp.validGroup(hand3, groupSize3);
 
     return 0;
 }
