d672f87832f91a9f7f6cfb30c994da718b82a5cd
Suanna <112144298+Suanna0323@users.noreply.github.com>
2025-03-23 21:08:39 -0700

    almost there

---
+++ Algorithm1.h
@@ -0,0 +1,121 @@
+// Suanna Shih
+// CPSC 335
+// Professor Sugandha Bhandari
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+class Array{
+  public:
+  //2D array find O, [i][j]
+  vector<vector<int>> locateSpawn(vector<vector<string>> arr)
+  {
+    vector<vector<int>> location;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[i].size(); j++){
+          if(arr[i][j] == "0"){
+            location.push_back({i,j});
+            //location[i].push_back(j);
+          }
+        }
+    }
+      return location;
+  }
+
+  //directions need for walking in spaces
+  const map<string, pair<int, int>> directions = {
+          {"up", {-1, 0}},
+          {"down", {1, 0}},
+          {"left", {0, -1}},
+          {"right", {0, 1}}
+    };
+
+  //takes in original map and location of spawn points
+  vector<vector<int>> stepMap(vector<vector<string>>& arr, vector<vector<int>> location)
+  {
+    //initialize things
+      int bound_i = arr[0].size();
+      int bound_j = arr.size();
+      vector<vector<int>> result(bound_i, vector<int>(bound_j));  //initialize an empty final map
+
+      // BFS
+      queue<pair<int, int>> q;  //visited spaces, since we're using BFS
+
+
+    //get spawn point in the queue
+      for (auto& loc : location)
+      { // start from spawn points
+        int r = loc[0];
+        int c = loc[1];
+        result[r][c] = 0;
+        q.push({r, c});
+      }
+
+      //BFS
+      int step = 0;
+      while (!q.empty()) // if there's divided point exist
+      {
+        pair<int, int> us = q.front();    //our current location(1st spawn point)
+        step = 1;
+        int i = us.first;
+        int j = us.second;
+        q.pop();
+
+        for (auto it = directions.begin(); it != directions.end(); ++it)  //look at all direction
+        {
+          //next spaces
+          int next_i = i + it->second.first;
+          int next_j = j + it->second.second;
+
+
+
+          if(next_i >= 0 && next_i < bound_i && next_j >= 0 && next_j < bound_j && arr[next_i][next_j] != "-1") //within bounds and not a wall
+          {
+            // if(arr[next_i][next_j] == "INF"){
+            //   arr[next_i][next_j] = step;
+            //   result[next_i][next_j] = step; //copy to result
+            //   q.push({next_i, next_j});
+            //   ++step;
+            // }
+
+            // //if step count is less, overwrite
+            if (arr[next_i][next_j] > arr[i][j])
+            {
+              arr[next_i][next_j] = arr[i][j];
+              result[i][j] = 1; //++step
+              q.push({next_i, next_j});   // move forward
+
+            }
+
+
+          }
+
+        }
+
+      }
+
+      return result;
+    }
+
+      void printArray(vector<vector<int>> arr)
+      {
+        for (int i = 0; i < arr.size(); i++)
+        {
+          for (int j = 0; j < arr[i].size(); j++)
+          {
+            cout << arr[i][j] << " ";
+          }
+          cout << endl;
+        }
+  }
+
+
+
+  private:
+  //help
+};
--- main.cpp
+++ main.cpp
@@ -1,35 +1,64 @@
-#include "VibeCheck.h"
+#include <iostream>
+#include <vector>
+#include "Algorithm1.h"
+#include "vibeCheck.h"
+using namespace std;
+int main(){
+  int choice;
+  cout << "Test algor (1,2)?" << endl;
+  cin >> choice;
+  switch(choice){
+    case 1:{
+      cout << "Test start!" << endl;
+      std::vector<vector<string>> walls = {{"INF", "-1", "-1", "INF"},
+                                           {"INF", "INF", "INF", "-1"},
+                                           {"INF", "-1", "INF", "-1"},
+                                           {"0", "-1", "INF", "INF"}};
+      std::vector<vector<string>> walls2 = {{"0", "INF", "INF"},
+                                            {"INF", "-1", "INF"},
+                                            {"INF", "INF", "0"}};
 
-int main()
-{
-    deckGroups temp;
-    cout << "Algorithm 2 Test: " << endl;   
-    //First Test (should be valid)
-    vector<int> hand = { 1, 2, 3, 6, 2, 3, 4, 7, 8 };
-    int groupSize = 3;
-    cout << "First Test [1, 2, 3, 6, 2, 3, 4, 7, 8]:\n";
-    temp.validGroup(hand, groupSize);
+      Array temp;
+      vector<vector<int>> spawn_point = temp.locateSpawn(walls2);
+      vector<vector<int>> result = temp.stepMap(walls2, spawn_point);
+      temp.printArray(result);
 
-    //Second Test (should be invalid)
-    cout << "\n";
-    vector<int> hand1 = { 1, 2, 3, 3, 4, 5, 6, 7 };
-    int groupSize1 = 4;
-    cout << "Second Test [1, 2, 3, 3, 4, 5, 6, 7]:\n";
-    temp.validGroup(hand1, groupSize1);
 
-    //Third Test (should be invalid)
-    cout << "\n";
-    vector<int> hand2 = { 1, 2, 3, 3, 4, 5, 6, 7 };
-    int groupSize2 = 5;
-    cout << "Third Test (Group Size 5) [1, 2, 3, 3, 4, 5, 6, 7]:\n";
-    temp.validGroup(hand2, groupSize2);
+      break;
+    }
 
-    //Fourth Test (should be valid)
-    cout << "\n";
-    vector<int> hand3 = {1, 3, 6, 2, 4, 2, 3, 8, 7};
-    int groupSize3 = 3;
-    cout << "Fourth Test [1, 3, 6, 2, 4, 2, 3, 8, 7]:\n";
-    temp.validGroup(hand3, groupSize3);
+    case 2:{
+      deckGroups temp;
+      cout << "Algorithm 2 Test: " << endl;
+      // First Test (should be valid)
+      vector<int> hand = {1, 2, 3, 6, 2, 3, 4, 7, 8};
+      int groupSize = 3;
+      cout << "First Test [1, 2, 3, 6, 2, 3, 4, 7, 8]:\n";
+      temp.validGroup(hand, groupSize);
 
-    return 0;
+      // Second Test (should be invalid)
+      cout << "\n";
+      vector<int> hand1 = {1, 2, 3, 3, 4, 5, 6, 7};
+      int groupSize1 = 4;
+      cout << "Second Test [1, 2, 3, 3, 4, 5, 6, 7]:\n";
+      temp.validGroup(hand1, groupSize1);
+
+      // Third Test (should be invalid)
+      cout << "\n";
+      vector<int> hand2 = {1, 2, 3, 3, 4, 5, 6, 7};
+      int groupSize2 = 5;
+      cout << "Third Test (Group Size 5) [1, 2, 3, 3, 4, 5, 6, 7]:\n";
+      temp.validGroup(hand2, groupSize2);
+
+      // Fourth Test (should be valid)
+      cout << "\n";
+      vector<int> hand3 = {1, 3, 6, 2, 4, 2, 3, 8, 7};
+      int groupSize3 = 3;
+      cout << "Fourth Test [1, 3, 6, 2, 4, 2, 3, 8, 7]:\n";
+      temp.validGroup(hand3, groupSize3);
+
+      return 0;
+      break;
+    }
+  }
 }
